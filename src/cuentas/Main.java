9c1f860b65ed4feca1a8b4cacc42ee28c9fa7f68
CeliaNTDev <celia.navarrete@gmail.com>
2022-02-16 14:41:07 +0100

    cambio Float to float

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -13,12 +13,13 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
+        float cantidad = 0;
 
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1, null);
+        operativa_cuenta(cuenta1, cantidad);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1, java.lang.Float cantidad) {
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         double saldoActual;
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual);
